280e27b5eb209e4e6d30189c6907e91955f3732c
Madeline Silva <mad.silva.ic@gmail.com>
2025-09-29 19:07:15 -0400

    add placeholders to sign in form

--- app/ui/signin.tsx
+++ app/ui/signin.tsx
@@ -27,8 +27,8 @@ export default function SignIn() {
     <div>
       <h2>sign in!!!</h2>
       <Form onSubmit={signIn}>
-        <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='email' />
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' />
         <button type='submit'>sign in!</button>
       </Form>
     </div>
