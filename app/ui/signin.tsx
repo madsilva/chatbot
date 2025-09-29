80b86c4443046ba2309343f402fc18342d9958b1
Madeline Silva <mad.silva.ic@gmail.com>
2025-09-29 19:06:46 -0400

    add working sign in form

--- app/ui/signin.tsx
+++ app/ui/signin.tsx
@@ -1,12 +1,36 @@
 import { useState } from 'react'
+import { Form } from 'react-router'
 import { authClient } from '../../utils/auth-client'
 
 export default function SignIn() {
   const [email, setEmail] = useState('')
-  const [name, setName] = useState('')
   const [password, setPassword] = useState('')
 
+  const signIn = async () => {
+    await authClient.signIn.email(
+      {email, password},
+      {
+        onRequest: (ctx) => {
+          //loading state here
+        },
+        onSuccess: (ctx) => {
+          // redirect to home
+        },
+        onError: (ctx) => {
+          alert(ctx.error)
+        }
+      }
+    )
+  }
+
   return (
-    <p>aaaa</p>
+    <div>
+      <h2>sign in!!!</h2>
+      <Form onSubmit={signIn}>
+        <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type='submit'>sign in!</button>
+      </Form>
+    </div>
   )
 }
